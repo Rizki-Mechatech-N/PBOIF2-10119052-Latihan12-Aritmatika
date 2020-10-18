001a611957363180bf9497e16598b1ab776c681e
Kiki <Kiki@Kiki-PC>
2020-10-18 11:45:31 +0700

    Upload Project

---
+++ src/pboif2/pkg10119052/latihan12/aritmatika/PBOIF210119052Latihan12Aritmatika.java
@@ -0,0 +1,35 @@
+package pboif2.pkg10119052.latihan12.aritmatika;
+
+
+/**
+ *
+ * NAMA   : Rizki Dwi nugraha
+ * KELAS  : IF-2
+ * NIM    : 10119052
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * operasi hitung aritmatika
+ * 
+ */
+public class PBOIF210119052Latihan12Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++ = " + (a++));
+        System.out.println("b-- = " + (a--));
+        System.out.println("d++ = " + (d++));
+        System.out.println("++d = " + (++d));
+    }
+    
+}
